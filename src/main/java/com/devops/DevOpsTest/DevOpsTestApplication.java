6bdb92eed570d2d3ccf19e87a9927ff44544c4c9
Your Name <you@example.com>
2024-02-20 10:44:07 +0800

    Init

---
+++ src/main/java/com/devops/DevOpsTest/DevOpsTestApplication.java
@@ -0,0 +1,13 @@
+package com.devops.DevOpsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/DevOpsTest/DevOpsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.DevOpsTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
