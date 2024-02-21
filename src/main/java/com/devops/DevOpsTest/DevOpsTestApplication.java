424718609e1b8ced26dadcb8dd9a497a88a7a809
Your Name <you@example.com>
2024-02-21 14:18:48 +0800

    change deployment file

--- src/main/java/com/devops/DevOpsTest/DevOpsTestApplication.java
+++ src/main/java/com/devops/DevOpsTest/DevOpsTestApplication.java
@@ -6,8 +6,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DevOpsTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevOpsTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DevOpsTestApplication.class, args);
 
+
+    }
+
+    private void check() {
+        var count = 1;
+        if (count > 1) {
+            return;
+        }
+
+        count++;
+
+        ////////
+
+
+        var count = 1;
+        if (count > 1) {
+            return;
+        } else {
+            count++;
+        }
+    }
 }
